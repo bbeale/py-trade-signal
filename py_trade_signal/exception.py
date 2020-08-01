c7032f276e81397cb9d204e304edf4306f57b52b
Ben <beale.ben@gmail.com>
2020-08-01 13:50:44 -0400

    WIP: Trade signal refactor -- Removed exception file in favor of defining a single exception class in __init__.py.

--- py_trade_signal/exception.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from pandas.errors import EmptyDataError, ParserError
-
-class TradeSignalException(EmptyDataError, ParserError, TypeError, ValueError, Exception):
-    pass
