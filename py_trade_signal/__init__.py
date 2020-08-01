5f5e21fecd2954c8d12b82c8039454129fbf27ac
Ben <beale.ben@gmail.com>
2020-08-01 13:55:49 -0400

    WIP: Trade signal refactor -- Handling validation of the first dataframe inside __init__ method. Added a validation function at this level for easy reuse.

--- py_trade_signal/__init__.py
+++ py_trade_signal/__init__.py
@@ -4,39 +4,30 @@ from .macd import MacdSignal
 from .mfi import MfiSignal
 from .vzo import VzoSignal
 import pandas as pd
-from pandas.errors import EmptyDataError, ParserError
 
 
 class TradeSignal(object):
 
-    def __init__(self, dataframe: pd.DataFrame):
+    def __init__(self,
+                 dataframe: pd.DataFrame,
+                 dataframe2: pd.DataFrame = None):
         """Initialize the TradeSignal class
 
         This class uses trading instrument agnostic technical indicators from peerchemist's finta Python module: https://github.com/peerchemist/finta
         """
         self.dataframe = dataframe
+        self.dataframe2 = dataframe2
 
-        if is_valid_dataframe(self.dataframe) is False:
-            try:
-                validate_dataframe(self.dataframe)
-            except EmptyDataError as err:
-                print('[?] A valid dataframe must be provided before any calculations can be performed')
-                raise err
-            except ParserError as err:
-                print('[!] A parsing error occurred')
-                raise err
+        if not is_valid_dataframe(self.dataframe):
+            raise TradeSignalException("[!] Signal calculations require at least one valid dataframe.")
 
-        self.macd_signal = MacdSignal()
-        self.mfi_signal = MfiSignal()
-        self.vzo_signal = VzoSignal()
+        self.macd_signal = MacdSignal(self.dataframe, self.dataframe2)
+        self.mfi_signal = MfiSignal(self.dataframe, self.dataframe2)
+        self.vzo_signal = VzoSignal(self.dataframe, self.dataframe2)
 
 
-def is_valid_dataframe(dataframe):
-    raise NotImplementedError
-
-
-def validate_dataframe(dataframe):
-    raise NotImplementedError
+def is_valid_dataframe(dataframe: pd.DataFrame) -> bool:
+    return dataframe is not None and len(dataframe.index) > 0
 
 
 class TradeSignalException(Exception):
