738e130422f4e29d2494385a614b655493abf91b
Ben <beale.ben@gmail.com>
2020-08-01 14:28:39 -0400

    Unit test placeholders.

---
+++ tests/test_mfi.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from py_trade_signal.mfi import MfiSignal, TradeSignalException
+import pytest
+
+
+def test_buy():
+    assert False
+
+
+def test_sell():
+    assert False
